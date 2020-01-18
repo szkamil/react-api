6d4c8f5702875c8b4c4fd69c852c52fec2f8d43e
Kamil <Kamil@gmail.com>
2020-01-18 18:34:29 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import Graph from './graph/graph.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            character: {}
+        }
+    }
+    
+    componentDidMount() {
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    character: data
+                })
+            })
+    }
+    
+    render() {
+        return (
+            <div>
+                {this.state.character.name}
+                <Graph/>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
+// class App extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//           loading: false, 
+//             character: {}
+//         }
+//     }
+    
+//     componentDidMount() {
+//       this.setState({loading: true})
+//         fetch("https://swapi.co/api/people/1")
+//             .then(response => response.json())
+//             .then(data => {
+//                 this.setState({
+//                     character: data,
+//                     loading: false
+//                 })
+//             })
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//               {this.state.character.name}
+//             </div>
+//         )
+//     }
+// }
+
+// export default App
+
+
+
+/**
+ * Other modern/advanced React features/topics to learn:
+ * 
+ * Official React Context API - https://reactjs.org/docs/context.html
+ * Error Boundaries - https://reactjs.org/docs/error-boundaries.html
+ * render props - https://reactjs.org/docs/render-props.html
+ * Higher Order Components - https://reactjs.org/docs/higher-order-components.html
+ * React Router - https://reacttraining.com/react-router/core/guides/philosophy
+ * React Hooks - https://reactjs.org/docs/hooks-intro.html
+ * React lazy, memo, and Suspense - https://reactjs.org/blog/2018/10/23/react-v-16-6.html
+ */
